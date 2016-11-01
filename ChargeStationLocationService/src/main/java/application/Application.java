2f2c5dae313a0617a0626be49d0525b2480308a2
ecarmik <mikael.carlsson@ericsson.com>
2016-11-01 09:55:10 +0100

    * Added basePackage

--- ChargeStationLocationService/src/main/java/application/Application.java
+++ ChargeStationLocationService/src/main/java/application/Application.java
@@ -1,7 +1,15 @@
 package application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackages={"services"})
+//@EnableAutoConfiguration
 
 @SpringBootApplication
 public class Application {
--- ChargeStationLocationService/src/main/java/services/ChargeStationRestController.java
+++ ChargeStationLocationService/src/main/java/services/ChargeStationRestController.java
@@ -2,6 +2,11 @@ package services;
 
 import java.util.ArrayList;
 import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +23,7 @@ import repository.ChargerTypeRepository;
 import repository.SupplierRepository;
 
 @RestController
-@RequestMapping("/chargestations")
+@RequestMapping("/{userId}/chargestations")
 public class ChargeStationRestController {
 
 	private final ChargeStationRepository chargeStationRepository;
@@ -26,7 +31,6 @@ public class ChargeStationRestController {
 	private final ChargerTypeRepository chargerTypeRepository;
 	private final SupplierRepository supplierRepository;
 
-	
 	public ChargeStationRestController(ChargeStationRepository chargeStationRepository, 
 			ChargerRepository chargerRepository,
 			ChargerTypeRepository chargerTypeRepository,
@@ -37,8 +41,9 @@ public class ChargeStationRestController {
 		this.chargerTypeRepository = chargerTypeRepository;
 	}
 	
+	
 	@RequestMapping(method = RequestMethod.GET)
-	Collection<ChargeStation> readChargeStations(@PathVariable String name) {
+	public Collection<ChargeStation> readChargestations(@PathVariable String userId) {
 		
 		Supplier s = new Supplier("Tesla");
 		supplierRepository.save(s);
@@ -59,3 +64,4 @@ public class ChargeStationRestController {
 		return this.chargeStationRepository.findAll();
 	}
 }
+
